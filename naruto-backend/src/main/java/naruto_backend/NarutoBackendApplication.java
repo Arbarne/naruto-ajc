425967b1d93aec5fe600b7784460decbff563033
bmoulard <baptiste.moulard@epitech.eu>
2026-07-03 13:28:43 +0200

    init front and back

---
+++ naruto-backend/src/main/java/naruto_backend/NarutoBackendApplication.java
@@ -0,0 +1,13 @@
+package naruto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutoBackendApplication.class, args);
+	}
+
+}

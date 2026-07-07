1f5aed1b14a7b9d5ae188e60312b059642dbf133
Cécile <ccilejouanne@yahoo.fr>
2026-07-07 09:12:35 +0200

    Création des classes Mission et Equipe

---
+++ naruto-backend/src/main/java/naruto_backend/model/Equipe.java
@@ -0,0 +1,85 @@
+package naruto_backend.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "team")
+public class Equipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "leader_id")
+    @Column(name = "leader", length = 70, nullable = false)
+    private Leader leader;
+
+    @OneToMany(mappedBy = "equipe")
+    private List<Ninja> ninjas;
+
+    @OneToMany(mappedBy = "equipe")
+    private List<Mission> missions;
+
+    
+    public Equipe() {
+    }
+
+    public Equipe(Integer id, Leader leader, List<Ninja> ninjas, List<Mission> missions) {
+        this.id = id;
+        this.leader = leader;
+        this.ninjas = ninjas;
+        this.missions = missions;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Leader getLeader() {
+        return leader;
+    }
+
+    public List<Ninja> getNinjas() {
+        return ninjas;
+    }
+
+    public List<Mission> getMissions() {
+        return missions;
+    }
+
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLeader(Leader leader) {
+        this.leader = leader;
+    }
+
+    public void setNinjas(List<Ninja> ninjas) {
+        this.ninjas = ninjas;
+    }
+
+    public void setMissions(List<Mission> missions) {
+        this.missions = missions;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Equipe [id=" + id + ", leader=" + leader + ", ninjas=" + ninjas + ", missions=" + missions + "]";
+    }
+    
+}

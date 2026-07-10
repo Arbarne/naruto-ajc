580c5f17675305e1989aac552748f1700698f35f
Damien <d.gervy@free.fr>
2026-07-10 10:55:20 +0200

    spring securité

--- naruto-backend/src/main/java/naruto_backend/service/EquipeService.java
+++ naruto-backend/src/main/java/naruto_backend/service/EquipeService.java
@@ -63,7 +63,7 @@ public class EquipeService {
 		daoEquipe.deleteById(id);
 	}
 
-    public Equipe findByLeaderId(Integer leaderId) {
+    public Optional<Equipe> findByLeaderId(Integer leaderId) {
         return daoEquipe.findByLeaderId(leaderId);
     }
 

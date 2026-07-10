d941800a26e8312e9047c38b71e54e0327b96784
Damien <d.gervy@free.fr>
2026-07-10 19:13:47 +0200

    login front fonctionnel

--- naruto-frontend/src/app/page/connexion-page/connexion-page.ts
+++ naruto-frontend/src/app/page/connexion-page/connexion-page.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion-page',
@@ -9,15 +11,39 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 })
 export class ConnexionPage {
   private formBuilder: FormBuilder = inject(FormBuilder);
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
 
+  private loginError = signal(false);
   protected usernameCtrl: FormControl = this.formBuilder.control('', Validators.required);
   protected passwordCtrl: FormControl = this.formBuilder.control('', Validators.required);
   protected connexionForm: FormGroup = this.formBuilder.group({
-    username: this.usernameCtrl,
+    login: this.usernameCtrl,
     password: this.passwordCtrl,
   });
 
   protected showPassword: boolean = false;
   protected usernameWalking: boolean = false;
   protected passwordWalking: boolean = false;
+
+  public async connecter() {
+    this.loginError.set(false);
+    this.authService.auth(this.connexionForm.getRawValue()).subscribe({
+      // next => si la réponse est OK
+      next: resp => {
+        if (resp.success == false) {
+          this.loginError.set(true);
+          return;
+        }
+
+        this.authService.token = resp.token;
+        this.authService.role = resp.role;
+        this.authService.equipeId = resp.equipeId;
+        this.router.navigate([ '/home' ]);
+      },
+
+      // error => si la réponse est KO (30X, 40X, 50X)
+      error: () => this.loginError.set(true)
+    });
+  }
 }
